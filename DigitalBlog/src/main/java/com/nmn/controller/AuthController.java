0c7173bdffdd0a6d3c122d38d84342622b3f9057
Nhat Nguyen Minh <intern.nmnhat2@tma.com.vn>
2024-03-26 10:59:36 +0700

    aa

--- DigitalBlog/src/main/java/com/nmn/controller/AuthController.java
+++ DigitalBlog/src/main/java/com/nmn/controller/AuthController.java
@@ -1,15 +1,28 @@
 package com.nmn.controller;
 
+import com.nmn.dto.AuthenticationDTO;
 import com.nmn.dto.UserDTO;
+import com.nmn.dto.response.AuthenticationResponse;
 import com.nmn.model.Users;
+import com.nmn.service.AuthService;
+import com.nmn.service.RedisService;
 import com.nmn.service.UserService;
+import com.nmn.service.impl.UserDetailsServiceImpl;
+import com.nmn.util.JwtUtil;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import java.security.Principal;
 import java.util.Map;
 
@@ -18,6 +31,13 @@ import java.util.Map;
 public class AuthController {
     @Autowired
     UserService  userService;
+
+
+    @Autowired
+    private RedisService redisService;
+
+    @Autowired
+    private AuthService authService;
 
 
     @Operation(summary = "Update profile", description = "Update profile")
@@ -46,5 +66,33 @@ public class AuthController {
     ResponseEntity<Users> getUser(Principal userAuth){
         Users user = userService.findUserByUserName(userAuth.getName());
         return  new ResponseEntity<>(user,HttpStatus.OK);
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticationResponse> createAuthenticationToken(@RequestBody AuthenticationDTO authenticationDTO, HttpServletResponse response) throws BadCredentialsException, DisabledException, UsernameNotFoundException, IOException {
+        String jwt = authService.login(authenticationDTO,response);
+        return new ResponseEntity<>(new AuthenticationResponse(jwt), HttpStatus.OK);
+
+    }
+    @SecurityRequirement(name = "Bearer Authentication")
+    @GetMapping(value = "/current-user",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Users> currentUser(Principal user){
+        Users u = userService.findUserByUserName(user.getName());
+        return new ResponseEntity<>(u, HttpStatus.OK);
+    }
+
+
+
+    @Operation(summary = "logout", description = "Log out")
+    @SecurityRequirement(name = "Bearer Authentication")
+    @GetMapping("/logout")
+    public AuthenticationResponse logout(Principal user){
+        Users userAuthentication = userService.findUserByUserName(user.getName());
+        if(user!= null){
+            redisService.deleteToken(userAuthentication.getId());
+            return new AuthenticationResponse("Logged out");
+        }
+        return new AuthenticationResponse("Log out fail");
     }
 }
